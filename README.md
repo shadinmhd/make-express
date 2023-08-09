0cb2ac2d15cb929feb300d9cdb0ff38999d37171
shadinmhd <108604275+shadinmhd@users.noreply.github.com>
2023-08-09 10:22:56 -0700

    added content to main file

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # make-express
 
-this is a simple app to generate express boilerplate code in javascript
+this is a simple package to generate express boilerplate code in javascript
 
 
 to install run this command
 ```
-npm install -g make-express
+    npm install -g make-express
 ```
+
+before using the app go to a folder you want to generate the files
 
 generate the app 
 ```
--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 #!/usr/bin/env node
 
 const { exec } = require("child_process")
-const {writeFileSync} = require("fs")
+const { writeFileSync } = require("fs")
 const { mkdirp } = require("mkdirp")
 
-const createDirectories =async () => {
+const appJsContent = `
+
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const morgan = require("morgan")
+const colors = require("colors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser())
+app.use(session({
+    secret : process.env.secret,
+    resave : false,
+    saveUninitialized : true
+}))
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(\`server started http://localhost:\${PORT}\`.cyan.bold)
+})
+`
+
+const packageJsonContent =
+    `
+{
+    "name": "dresser",
+    "version": "1.0.0",
+    "description": "",
+    "main": "main.js",
+    "scripts": {
+        "start" : "nodemon" 
+    },
+    "keywords": [],
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+      "colors": "latest",
+      "cookie-parser": "latest",
+      "dotenv": "latest",
+      "express": "latest",
+      "express-session": "latest",
+      "morgan": "latest",
+      "nodemon" : "latest"
+    }
+}
+  
+`
+
+const createDirectories = async () => {
     await mkdirp("./routes")
     await mkdirp("./public")
     await mkdirp("./middlewares")
@@ -13,20 +66,17 @@ const createDirectories =async () => {
 }
 
 const createMainFile = () => {
-    writeFileSync("./main.js","",(err) => {
-        if(err){
-            console.log(err)
-        }
-    })        
+    writeFileSync("./main.js", appJsContent)
 }
 
-try{
-    exec("npm init -y", (err) => console.log(err))
+try {
+    exec("npm init -y")
+    writeFileSync("./package.json", packageJsonContent)
     createDirectories()
     createMainFile()
     console.log(`
-        npm install
+    install packages> npm install
+    start server> npm start
     `)
-}catch(err){
-    console.log(err)
+} catch (err) {
 }
